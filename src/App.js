537d374d99664cb3cda1805456e002b89f66c9a7
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2023-08-08 22:01:59 -0500

    add lazy and suspend

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 
 import './index.css';
-import Artists from './Artists';
+//import Artists from './Artists';
+
+import { Suspense, lazy } from 'react';
+const Artists = lazy(() => import('./Artists'))
+
 
 function App() {
   return (
     <div className="App">
-      <Artists />
+      <Suspense fallback={<h1>Still Loading…</h1>}>
+        <Artists />
+      </Suspense>
     </div>
   );
 }
