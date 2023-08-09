d57275f254545bd869d6d9eca6e59d635ef66793
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2023-08-08 21:56:20 -0500

    first init

---
+++ src/Artists.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import artists from "./Store";
+
+export default function Artists() {
+ return (
+   <>
+    <h1>MTV Base Headline Artists 2019</h1>
+    {artists.map(artist =>(
+      <div id='card-body' key={artist.id}>
+        <div className='card'>
+        <h2>{artist.name}</h2>
+        <p>genre: {artist.genre}</p>
+        <p>Albums released: {artist.albums}</p>
+        </div>
+      </div>
+    ))}
+   </>
+);
+}
