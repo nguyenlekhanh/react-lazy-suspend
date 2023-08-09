c55b2f76ff03097fe0b893f6bfd6889c71f9cfc8
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2023-08-08 22:10:44 -0500

    add second lazy component

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import './index.css';
 
 import { Suspense, lazy } from 'react';
 const Artists = lazy(() => import('./Artists'))
-
+const Performers = lazy(() => import('./performers'))
 
 function App() {
   return (
     <div className="App">
       <Suspense fallback={<h1>Still Loading…</h1>}>
         <Artists />
+        <Performers />
       </Suspense>
     </div>
   );
